57c257e5d07f9fa67b47658cc8d8c202468aabb6
Christian Lopez <me@ralkage.com>
2023-02-03 09:44:26 -0500

    Update test case

--- cypress/e2e/cy_origin_cucm_test.cy.js
+++ cypress/e2e/cy_origin_cucm_test.cy.js
@@ -1,10 +1,10 @@
-it("does not work", () => {
-  cy.visit("https://192.168.124.70")
+it("cy.origin does not work for CUCM in Cypress version 12.4.1", () => {
   cy.origin("https://192.168.110.192", () => {
     cy.visit("/")
     cy.contains("Cisco Unified Communications Manager").click()
     cy.get('[name="j_username"]').type("test123")
     cy.get('[name="j_password"]').type("test123")
     cy.get("form").submit()
+    cy.wait(2000)
   })
 })
